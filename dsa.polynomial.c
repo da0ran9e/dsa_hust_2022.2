baa5d31e5c8a2e3bc9280e00e23ee678f46766d2
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-04-15 14:46:28 +0700

    delete exe

--- dsa.polynomial.c
+++ dsa.polynomial.c
@@ -4,7 +4,7 @@
 #define max(a,b) (((a)>(b))?(a):(b))
 
 
-int poly_id,poly_id1, poly_id2, coef, Exp, variable_value, result_poly_id;
+
 typedef struct{
 	int c;
 	int e;
@@ -24,21 +24,21 @@ int pow(int n, int x){
 }
 
 void sort(int index){
-	for(int i=0; i<terms[index]-1; i++){
-		for (int j=0; j<terms[index]-i-1; j++){
-			if(poly[index][i+1].e> poly[index][j+1].e){
-				Terms temp = poly[index][i+1];
-				poly[index][i+1] = poly[index][j+1];
+	//printf("%d %d\n", poly[3][1].c, poly[3][1].e);
+	for(int i=1; i<=terms[index]; i++){
+		for (int j=1; j<=terms[index]-i; j++){
+			if(poly[index][j].e < poly[index][j+1].e){
+				Terms temp = poly[index][j];
+				poly[index][j] = poly[index][j+1];
 				poly[index][j+1] = temp;
-
 			}
-			
 		}
 	}
 }
 
 int main( ){
 	char command[100];
+	int poly_id,poly_id1, poly_id2, coef, Exp, variable_value, result_poly_id;
 
 	do{
 		scanf("%s", command);
@@ -61,7 +61,6 @@ int main( ){
 			}
 			
 			if(exist[poly_id]!=1) exist[poly_id]=1;
-			//printf("d %d %d %d \n", poly[poly_id][terms[poly_id]].c, poly[poly_id][terms[poly_id]].e, terms[poly_id]);
 		}
 		else if(strcmp(command, "EvaluatePoly")==0 ){
 			scanf("%d %d", &poly_id, &variable_value);
@@ -86,7 +85,6 @@ int main( ){
 			}
 			for(int i=1; i<=terms[poly_id2]; i++){
 				if(pows[result_poly_id][poly[poly_id2][i].e]=='\0'){
-					//printf("%d\n", poly[poly_id2][i].c);
 					terms[result_poly_id]++;
 					pows[result_poly_id][poly[poly_id2][i].e]=terms[result_poly_id];
 					poly[result_poly_id][terms[result_poly_id]].c = poly[poly_id2][i].c;
@@ -100,8 +98,8 @@ int main( ){
 		}
 		else if(strcmp(command, "PrintPoly")==0){
 			scanf("%d", &poly_id);
+			//printf("%d %d\n", poly[3][1].c, poly[3][1].e);
 			sort(poly_id);
-			//printf("Poly %d", terms[3]);
 			for(int i=1; i<=terms[poly_id]; i++){
 				printf("%d %d ", poly[poly_id][i].c, poly[poly_id][i].e);
 			}
